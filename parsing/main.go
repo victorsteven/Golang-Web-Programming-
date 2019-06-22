881e386040af9b381141ddb1fda237fa789e13c4
steven victor <chikodi543@gmail.com>
2019-06-22 23:29:13 +0100

    Parse a string to template

--- parsing/main.go
+++ parsing/main.go
@@ -9,11 +9,14 @@ import (
 var tpl *template.Template
 
 func init() {
+	//using Must, we automatically check our errors, so no need of assigning an error variable
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
 func main() {
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
+	// err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", "This is a string we parsed here")
+
 	if err != nil {
 		log.Fatalln(err)
 	}
