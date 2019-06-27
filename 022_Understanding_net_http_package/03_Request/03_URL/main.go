27328cb5d1145f36c2b6ddadfb49cd8cb9f5aa17
steven victor <chikodi543@gmail.com>
2019-06-27 14:08:28 +0100

    Understanding request

---
+++ 022_Understanding_net_http_package/03_Request/03_URL/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method      string
+		URL         *url.URL
+		Submissions url.Values
+	}{
+		req.Method,
+		req.URL,
+		req.Form,
+	}
+	tpl.ExecuteTemplate(res, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":7000", d)
+}
---
+++ 022_Understanding_net_http_package/03_Request/04_Header/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type hotdog int
+
+func (m hotdog) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method      string
+		URL         *url.URL
+		Submissions map[string][]string
+		Header      http.Header
+	}{
+		req.Method,
+		req.URL,
+		req.Form,
+		req.Header,
+	}
+	tpl.ExecuteTemplate(res, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":7000", d)
+}
