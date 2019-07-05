d8c9a115e268d9577b5f856f861581cfe5b4e64c
steven victor <chikodi543@gmail.com>
2019-07-05 13:48:00 +0100

    Implement login

--- 031_Sessions/04_Signup/main.go
+++ 031_Sessions/04_Signup/main.go
@@ -22,12 +22,15 @@ var dbSessions = map[string]string{}
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
+	bs, _ := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.MinCost)
+	dbUsers["victorsteven"] = user{"victorsteven", bs, "Victor", "Steven"}
 }
 
 func main() {
 	http.HandleFunc("/", index)
 	http.HandleFunc("/bar", bar)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":7000", nil)
 }
@@ -88,4 +91,40 @@ func signup(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 	tpl.ExecuteTemplate(res, "signup.gohtml", nil)
+}
+
+func login(res http.ResponseWriter, req *http.Request) {
+	if alreadyLoggedIn(req) {
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+	//process form submission
+	if req.Method == http.MethodPost {
+		un := req.FormValue("username")
+		p := req.FormValue("password")
+
+		//is there a username:
+		u, ok := dbUsers[un]
+		if !ok {
+			http.Error(res, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		//does the entered password match the stored password?
+		err := bcrypt.CompareHashAndPassword(u.Password, []byte(p))
+		if err != nil {
+			http.Error(res, "Username and/or password do not match", http.StatusForbidden)
+			return
+		}
+		//create session
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(res, c)
+		dbSessions[c.Value] = un
+		http.Redirect(res, req, "/", http.StatusSeeOther)
+		return
+	}
+	tpl.ExecuteTemplate(res, "login.gohtml", nil)
 }
